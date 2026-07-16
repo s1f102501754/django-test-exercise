a65d6aadebadfc81f008d115541bdeecf6003542
1f10250175 <s1f102501754@iniad.org>
2026-07-16 14:03:14 +0900

    Editの編集

--- todo/views.py
+++ todo/views.py
@@ -3,6 +3,7 @@ from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
 from todo.models import Task
+from django.utils import timezone
 
 
 # Create your views here.
@@ -47,14 +48,34 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
+
+    error_message = None
+
     if request.method == 'POST':
-        task.title = request.POST['title']
-        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
-        task.save()
-        return redirect(detail, task_id)
+        title = request.POST.get('title', '')
+        due_at_raw = request.POST.get('due_at', '')
+        due_at = make_aware(parse_datetime(due_at_raw)) if due_at_raw else None
+
+        # バリデーションチェック
+        if not title.strip():
+            error_message = "Title cannot be empty!"
+        elif due_at and due_at < timezone.now():
+            error_message = "Due date cannot be in the past!"
+        
+        # エラーがなければタスクに代入して保存
+        if not error_message:
+            task.title = title
+            task.due_at = due_at
+            
+            # completed チェックボックスの判定（送られてきていれば True）
+            task.completed = 'completed' in request.POST
+            
+            task.save()
+            return redirect(detail, task_id)
 
     context = {
-        'task': task
+        'task': task,
+        'error_message': error_message,
     }
     return render(request, 'todo/edit.html', context)
 
